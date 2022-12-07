8e102d88ba34905438628c4451454cedccdc1ec4
AMISHA JHA <106396122+a-jha-06@users.noreply.github.com>
2022-12-07 06:23:12 +0000

    Pending changes exported from your codespace

--- src/App.js
+++ src/App.js
@@ -1,29 +1,76 @@
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import Search from "./components/Search";
+import Results from "./components/Results";
+import Detail from "./components/Detail";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
-  );
+const [state, setState] = useState({
+	s: "sherlock",
+	results: [],
+	selected: {},
+});
+
+const apiurl = "https://www.omdbapi.com/?apikey=a2526df0";
+
+const searchInput = (e) => {
+	let s = e.target.value;
+
+	setState((prevState) => {
+	return { ...prevState, s: s };
+	});
+};
+
+const search = (e) => {
+	if (e.key === "Enter") {
+	axios(apiurl + "&s=" + state.s).then(({ data }) => {
+		let results = data.Search;
+
+		console.log(results);
+
+		setState((prevState) => {
+		return { ...prevState, results: results };
+		});
+	});
+	}
+};
+
+const openDetail = (id) => {
+	axios(apiurl + "&i=" + id).then(({ data }) => {
+	let result = data;
+
+	setState((prevState) => {
+		return { ...prevState, selected: result };
+	});
+	});
+};
+
+const closeDetail = () => {
+	setState((prevState) => {
+	return { ...prevState, selected: {} };
+	});
+};
+
+return (
+	<div className="App">
+	<header className="App-header">
+		<h1>Movie Hub</h1>
+	</header>
+	<main>
+		<Search searchInput={searchInput} search={search} />
+
+		<Results results={state.results} openDetail={openDetail} />
+
+		{typeof state.selected.Title != "undefined" ? (
+		<Detail selected={state.selected} closeDetail={closeDetail} />
+		) : (
+		false
+		)}
+	</main>
+	</div>
+);
 }
 
 export default App;
+
